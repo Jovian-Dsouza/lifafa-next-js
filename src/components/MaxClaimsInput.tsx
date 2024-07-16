b75844ac4484c4059541917ddcc8d984df5433b7
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-07-16 19:24:43 +0000

    feat: add max claims input

---
+++ src/components/MaxClaimsInput.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { UsersIcon } from "lucide-react";
+
+interface MaxClaimsInputProps {
+  maxClaims: number;
+  onChangeMaxClaims: (value: number) => void;
+}
+
+export const MaxClaimsInput: React.FC<MaxClaimsInputProps> = ({
+  maxClaims,
+  onChangeMaxClaims,
+}) => {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = parseInt(e.target.value);
+    if (!isNaN(value) && value >= 0) {
+      onChangeMaxClaims(value);
+    } else {
+      onChangeMaxClaims(0);
+    }
+  }
+
+  return (
+    <div className="relative mb-2">
+      <input
+        type="number"
+        value={maxClaims}
+        onChange={handleChange}
+        placeholder="Add max no. of recipients"
+        className="bg-white border border-gray-200 px-4 py-2 rounded-lg focus:border-blue-600 w-full no-scrollbar"
+        min="0"
+      />
+      <div className="absolute right-4 bottom-3">
+        <UsersIcon className="text-black" width={20} height={20} />
+      </div>
+    </div>
+  );
+};
