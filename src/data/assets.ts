d968c70631a390118a7027132fdf75246151d9df
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-07-16 15:44:16 +0000

    feat: add data constants

---
+++ src/data/assets.ts
@@ -0,0 +1,15 @@
+export const images = {
+  lifafa: "/lifafa.png",
+  okto: "/okto.png",
+  tokens: {
+    bonk: "/tokens/bonk.jpeg",
+    sol: "/tokens/sol.jpeg",
+    usdc: "/tokens/usdc.jpeg",
+    wif: "/tokens/wif.jpeg",
+  },
+  envelope: {
+    bottom: "/envelope/bottom.png",
+    top: "/envelope/top.png",
+    star: "/envelope/star.png",
+  },
+};
---
+++ src/data/constants.ts
@@ -0,0 +1,18 @@
+import { images } from "./assets";
+
+export const tokens = [
+  {
+    name: "SOL",
+    symbol: "SOL",
+    icon: images.tokens.sol,
+    blockchain: "Solana",
+    blockchainIcon: images.tokens.sol,
+  },
+  {
+    name: "USDC",
+    symbol: "USDC",
+    icon: images.tokens.usdc,
+    blockchain: "Solana",
+    blockchainIcon: images.tokens.sol,
+  },
+];
