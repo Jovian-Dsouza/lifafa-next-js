7e1180e9682d6c67fb2b8f709334d32a25cda974
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-07-21 12:52:08 +0000

    feat: upgrade lifafa contract

--- src/contracts/lifafa.ts
+++ src/contracts/lifafa.ts
@@ -1,24 +1,177 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/lifafa.json`.
+ */
 export type Lifafa = {
-  "version": "0.1.0",
-  "name": "lifafa",
+  "address": "2h4ToTTMT93ApLUHGqsvFofsD9JSRdLBsMpAxohkgT12",
+  "metadata": {
+    "name": "lifafa",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
   "instructions": [
     {
-      "name": "createSolLifafa",
+      "name": "claimSolLifafa",
+      "discriminator": [
+        209,
+        19,
+        123,
+        104,
+        109,
+        66,
+        48,
+        71
+      ],
       "accounts": [
         {
           "name": "lifafa",
-          "isMut": true,
-          "isSigner": false
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  108,
+                  105,
+                  102,
+                  97,
+                  102,
+                  97
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "id"
+              }
+            ]
+          }
+        },
+        {
+          "name": "userClaim",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  117,
+                  115,
+                  101,
+                  114,
+                  95,
+                  99,
+                  108,
+                  97,
+                  105,
+                  109
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "lifafa"
+              },
+              {
+                "kind": "account",
+                "path": "signer"
+              }
+            ]
+          }
         },
         {
           "name": "signer",
-          "isMut": true,
-          "isSigner": true
+          "writable": true,
+          "signer": true
         },
         {
           "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "id",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "createSolLifafa",
+      "discriminator": [
+        204,
+        69,
+        222,
+        66,
+        135,
+        118,
+        64,
+        172
+      ],
+      "accounts": [
+        {
+          "name": "lifafa",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  108,
+                  105,
+                  102,
+                  97,
+                  102,
+                  97
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "id"
+              }
+            ]
+          }
+        },
+        {
+          "name": "userClaim",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  117,
+                  115,
+                  101,
+                  114,
+                  95,
+                  99,
+                  108,
+                  97,
+                  105,
+                  109
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "lifafa"
+              },
+              {
+                "kind": "account",
+                "path": "signer"
+              }
+            ]
+          }
+        },
+        {
+          "name": "signer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
         }
       ],
       "args": [
@@ -36,7 +189,7 @@ export type Lifafa = {
         },
         {
           "name": "maxClaims",
-          "type": "u16"
+          "type": "u64"
         },
         {
           "name": "ownerName",
@@ -49,43 +202,45 @@ export type Lifafa = {
       ]
     },
     {
-      "name": "claimSolLifafa",
-      "accounts": [
-        {
-          "name": "lifafa",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "signer",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "id",
-          "type": "u64"
-        }
-      ]
-    },
-    {
       "name": "deleteSolLifafa",
+      "discriminator": [
+        188,
+        252,
+        75,
+        30,
+        255,
+        168,
+        84,
+        151
+      ],
       "accounts": [
         {
           "name": "lifafa",
-          "isMut": true,
-          "isSigner": false
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  108,
+                  105,
+                  102,
+                  97,
+                  102,
+                  97
+                ]
+              },
+              {
+                "kind": "arg",
+                "path": "id"
+              }
+            ]
+          }
         },
         {
           "name": "signer",
-          "isMut": true,
-          "isSigner": true
+          "writable": true,
+          "signer": true
         }
       ],
       "args": [
@@ -97,6 +252,71 @@ export type Lifafa = {
     }
   ],
   "accounts": [
+    {
+      "name": "lifafa",
+      "discriminator": [
+        50,
+        94,
+        121,
+        47,
+        119,
+        66,
+        206,
+        200
+      ]
+    },
+    {
+      "name": "userClaim",
+      "discriminator": [
+        228,
+        142,
+        195,
+        181,
+        228,
+        147,
+        32,
+        209
+      ]
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "timeLimitExpired",
+      "msg": "TimeLimit has expired"
+    },
+    {
+      "code": 6001,
+      "name": "unauthorized",
+      "msg": "unauthorized"
+    },
+    {
+      "code": 6002,
+      "name": "alreadyClaimed",
+      "msg": "Already claimed"
+    },
+    {
+      "code": 6003,
+      "name": "maxClaimsReached",
+      "msg": "Max Claims Reached"
+    },
+    {
+      "code": 6004,
+      "name": "maxClaimsLimitExceeded",
+      "msg": "Max claims limit exceeded"
+    },
+    {
+      "code": 6005,
+      "name": "ownerNameTooLong",
+      "msg": "Owner name too long"
+    },
+    {
+      "code": 6006,
+      "name": "descriptionTooLong",
+      "msg": "Description too long"
+    }
+  ],
+  "types": [
     {
       "name": "lifafa",
       "type": {
@@ -116,30 +336,23 @@ export type Lifafa = {
           },
           {
             "name": "owner",
-            "type": "publicKey"
+            "type": "pubkey"
           },
           {
             "name": "ownerName",
             "type": "string"
           },
           {
-            "name": "claimed",
-            "type": {
-              "vec": {
-                "array": [
-                  "u8",
-                  8
-                ]
-              }
-            }
+            "name": "claims",
+            "type": "u64"
           },
           {
             "name": "maxClaims",
-            "type": "u16"
+            "type": "u64"
           },
           {
             "name": "mintOfTokenBeingSent",
-            "type": "publicKey"
+            "type": "pubkey"
           },
           {
             "name": "amount",
@@ -151,239 +364,22 @@ export type Lifafa = {
           }
         ]
       }
-    }
-  ],
-  "errors": [
-    {
-      "code": 6000,
-      "name": "TimeLimitExpired",
-      "msg": "TimeLimit has expired"
     },
     {
-      "code": 6001,
-      "name": "Unauthorized",
-      "msg": "Unauthorized"
-    },
-    {
-      "code": 6002,
-      "name": "AlreadyClaimed",
-      "msg": "Already claimed"
-    },
-    {
-      "code": 6003,
-      "name": "MaxClaimsReached",
-      "msg": "Max Claims Reached"
-    },
-    {
-      "code": 6004,
-      "name": "MaxClaimsLimitExceeded",
-      "msg": "Max claims limit exceeded"
-    },
-    {
-      "code": 6005,
-      "name": "OwnerNameTooLong",
-      "msg": "Owner name too long"
-    },
-    {
-      "code": 6006,
-      "name": "DescriptionTooLong",
-      "msg": "Description too long"
-    }
-  ]
-};
-
-export const IDL: Lifafa = {
-  "version": "0.1.0",
-  "name": "lifafa",
-  "instructions": [
-    {
-      "name": "createSolLifafa",
-      "accounts": [
-        {
-          "name": "lifafa",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "signer",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "id",
-          "type": "u64"
-        },
-        {
-          "name": "amount",
-          "type": "u64"
-        },
-        {
-          "name": "timeLimitInSeconds",
-          "type": "i64"
-        },
-        {
-          "name": "maxClaims",
-          "type": "u16"
-        },
-        {
-          "name": "ownerName",
-          "type": "string"
-        },
-        {
-          "name": "desc",
-          "type": "string"
-        }
-      ]
-    },
-    {
-      "name": "claimSolLifafa",
-      "accounts": [
-        {
-          "name": "lifafa",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "signer",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "id",
-          "type": "u64"
-        }
-      ]
-    },
-    {
-      "name": "deleteSolLifafa",
-      "accounts": [
-        {
-          "name": "lifafa",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "signer",
-          "isMut": true,
-          "isSigner": true
-        }
-      ],
-      "args": [
-        {
-          "name": "id",
-          "type": "u64"
-        }
-      ]
-    }
-  ],
-  "accounts": [
-    {
-      "name": "lifafa",
+      "name": "userClaim",
       "type": {
         "kind": "struct",
         "fields": [
           {
-            "name": "id",
-            "type": "u64"
-          },
-          {
-            "name": "creationTime",
-            "type": "i64"
-          },
-          {
-            "name": "timeLimit",
-            "type": "i64"
-          },
-          {
-            "name": "owner",
-            "type": "publicKey"
-          },
-          {
-            "name": "ownerName",
-            "type": "string"
-          },
-          {
             "name": "claimed",
-            "type": {
-              "vec": {
-                "array": [
-                  "u8",
-                  8
-                ]
-              }
-            }
+            "type": "bool"
           },
           {
-            "name": "maxClaims",
-            "type": "u16"
-          },
-          {
-            "name": "mintOfTokenBeingSent",
-            "type": "publicKey"
-          },
-          {
-            "name": "amount",
+            "name": "amountClaimed",
             "type": "u64"
-          },
-          {
-            "name": "desc",
-            "type": "string"
           }
         ]
       }
     }
-  ],
-  "errors": [
-    {
-      "code": 6000,
-      "name": "TimeLimitExpired",
-      "msg": "TimeLimit has expired"
-    },
-    {
-      "code": 6001,
-      "name": "Unauthorized",
-      "msg": "Unauthorized"
-    },
-    {
-      "code": 6002,
-      "name": "AlreadyClaimed",
-      "msg": "Already claimed"
-    },
-    {
-      "code": 6003,
-      "name": "MaxClaimsReached",
-      "msg": "Max Claims Reached"
-    },
-    {
-      "code": 6004,
-      "name": "MaxClaimsLimitExceeded",
-      "msg": "Max claims limit exceeded"
-    },
-    {
-      "code": 6005,
-      "name": "OwnerNameTooLong",
-      "msg": "Owner name too long"
-    },
-    {
-      "code": 6006,
-      "name": "DescriptionTooLong",
-      "msg": "Description too long"
-    }
   ]
 };
-
-export const programId = "AXBZp473xo61tmpSryZfuZMGBs2c1q2MdVh2yW7GLS1z";
