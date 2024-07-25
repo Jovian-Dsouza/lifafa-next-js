6b4da7457866feb2e1cc0daa97de0bc97c33d68e
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-07-25 11:49:37 +0000

    fix: remove token balance

--- src/components/createLifafaComponent.tsx
+++ src/components/createLifafaComponent.tsx
@@ -105,7 +105,7 @@ export const CreateLifafaComponent = () => {
     <div className="w-[22rem] bg-[#F5F6FE]  rounded-3xl p-4 shadow">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold text-black">Create Lifafa</h2>
-        <TokenBalance token={selectedToken} />
+        {/* <TokenBalance token={selectedToken} /> */}
       </div>
 
       {/* Token Selector and price */}
