1c26ac2492a79b89738bf0982e662b0cb4560e7f
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-05 14:54:33 +0000

    fix: update footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -12,8 +12,8 @@ import Logo from "../../public/Lifafa-Icon.svg";
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+      <div className="container flex flex-col items-center justify-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-row items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Image src={Logo} alt="Logo" />
           <p className="text-center text-sm leading-loose md:text-left">
             Built by{" "}
@@ -28,9 +28,6 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             .
           </p>
         </div>
-        {/* <nav className="flex items-center gap-2">
-          <ThemeModeToggle />
-        </nav> */}
       </div>
     </footer>
   );
